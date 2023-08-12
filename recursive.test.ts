6d3d6a5a734143480e883a6e046a858f4a552f83
valentin-gavela <valentin.gavela@despegar.com>
2023-08-12 14:09:29 -0300

    1 file modified

--- recursive.test.ts
+++ recursive.test.ts
@@ -40,3 +40,26 @@ describe('isPalindrome', () => {
     expect(isPalindrome('awesome')).toBe(false);
   });
 });
+
+// ----------
+const isOdd = (val: number) => val % 2 !== 0;
+
+function someRecursive(nums: number[], fn: (...args: any) => boolean) {
+  // basecase
+  if (nums.length === 0) return false;
+
+  const [numToTest, ...rest] = nums;
+
+  if (fn(numToTest)) return true;
+
+  return someRecursive(rest, fn);
+}
+
+describe('someRecursive', () => {
+  it('should returns true if the callback fn returns true', () => {
+    expect(someRecursive([1, 2, 3, 4], isOdd)).toBe(true);
+    expect(someRecursive([4, 6, 8, 9], isOdd)).toBe(true);
+    expect(someRecursive([4, 6, 8], isOdd)).toBe(false);
+    expect(someRecursive([4, 6, 8], (val) => val > 10)).toBe(false);
+  });
+});
