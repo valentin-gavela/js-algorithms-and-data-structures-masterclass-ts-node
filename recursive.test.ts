a6d3cc0d227e897386bf5ffb631be2649814ed33
valentin-gavela <valentin.gavela@despegar.com>
2023-08-12 14:11:07 -0300

    1 file modified

--- recursive.test.ts
+++ recursive.test.ts
@@ -63,3 +63,21 @@ describe('someRecursive', () => {
     expect(someRecursive([4, 6, 8], (val) => val > 10)).toBe(false);
   });
 });
+
+// TODO flatten
+function flatten(oldArr) {
+  var newArr = [];
+  for (var i = 0; i < oldArr.length; i++) {
+    if (Array.isArray(oldArr[i])) {
+      newArr = newArr.concat(flatten(oldArr[i]));
+    } else {
+      newArr.push(oldArr[i]);
+    }
+  }
+  return newArr;
+}
+
+// flatten([1, 2, 3, [4, 5] ]) // [1, 2, 3, 4, 5]
+// flatten([1, [2, [3, 4], [[5]]]]) // [1, 2, 3, 4, 5]
+// flatten([[1],[2],[3]]) // [1,2,3]
+// flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]) // [1,2,3
