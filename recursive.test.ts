196a656ea19abf23d0ede501b7e858acf4844208
valentin-gavela <valentin.gavela@despegar.com>
2023-08-12 13:59:28 -0300

    1 file modified, 1 added

--- recursive.test.ts
+++ recursive.test.ts
@@ -2,12 +2,41 @@ function reverse(input: string) {
   // basecase
   if (input.length === 1) return input;
 
-  return reverse(input.at(-1)) + reverse(input.slice(0, -1));
+  return reverse(input.slice(-1)) + reverse(input.slice(0, -1));
 }
 
 describe('reverse', () => {
   it('should reverse', () => {
     expect(reverse('awesome')).toBe('emosewa');
     expect(reverse('rithmschool')).toBe('loohcsmhtir');
+  });
+});
+
+// ------
+
+function isPalindrome(input: string) {
+  // basecase
+  if (input.length <= 1) {
+    return true;
+  }
+
+  const isEqual = input[0] === input.slice(-1);
+  if (isEqual) {
+    isPalindrome(input.slice(1, -1));
+
+    return true;
+  }
+
+  return false;
+}
+
+describe('isPalindrome', () => {
+  it('should returns correct result', () => {
+    expect(isPalindrome('tacocat')).toBe(true);
+    expect(isPalindrome('awesome')).toBe(false);
+    expect(isPalindrome('foobar')).toBe(false);
+    expect(isPalindrome('amanaplanacanalpanama')).toBe(true);
+    expect(isPalindrome('amanaplanacanalpandemonium')).toBe(false);
+    expect(isPalindrome('awesome')).toBe(false);
   });
 });
